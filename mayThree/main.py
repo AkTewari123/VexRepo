70003dd34ed7732e46a316ea956892289c202c93
Messef <85040583+Messef@users.noreply.github.com>
2024-05-03 10:13:11 -0400

    Create main.py
    Mr. Hanas's code for doing git commits and branches

---
+++ mayThree/main.py
@@ -0,0 +1,12 @@
+"""
+Name: main.py
+Coded: A. Tewari
+Purpose: How to use branches + commits
+Date: 5/3/24
+"""
+
+counter = 0
+
+while counter < 10:
+    print(f"Time: {counter}")
+    counter += 4
